d02b05b4f2514ac59cae75656823885820f302c0
fahmifc123 <f4hm126@yahoo.co.id>
2018-07-07 17:55:58 +0700

    setting delay

--- codesensorgetar/codesensorgetar.ino
+++ codesensorgetar/codesensorgetar.ino
@@ -12,7 +12,7 @@ void loop(){
   delay(50);
  // Serial.print("measurment = ");
   Serial.println(measurement);
-  if (measurement > 100){
+  if (measurement > 200){
     digitalWrite(LED_Pin, HIGH);
   }
   else{
