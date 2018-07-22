8e5280e0d21c4bb7a79c78eeca3d063050bb1f2d
fahmifc123 <f4hm126@yahoo.co.id>
2018-07-22 14:09:57 +0700

    sensor getar

--- codesensorgetar/codesensorgetar.ino
+++ codesensorgetar/codesensorgetar.ino
@@ -5,7 +5,7 @@ void setup(){
   pinMode(LED_Pin, OUTPUT);
   pinMode(vibr_Pin, INPUT); //set vibr_Pin input for measurment
   Serial.begin(9600); //init serial 9600
- // Serial.println("----------------------Vibration demo------------------------");
+ // Serial.println(......);
 }
 void loop(){
   long measurement =TP_init();
